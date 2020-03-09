4d32b381a8a128b80b7466aaeeb6f8b4fdb8d5c9
Renat <suleymanov_renat@mail.ru>
2020-03-09 23:17:10 +0300

    задача 1

--- src/add.js
+++ src/add.js
@@ -1,15 +1,72 @@
-export default Character;
+export {Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon};
 
-function Character(name, type) {
-  this.name = name;
-  this.type = type;
-  this.health = 100;
-  this.attack = 10;
-  this.defence = 40;
-
-  this.damage = (points) => {
+class Character {
+  constructor(name, type, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.attack = attack;
+    this.defence = defence;
+  }
+  damage(points) {
     if (this.health >= 0) {
       this.health -= points * (1 - this.defence / 100);
     }
   }
 }
+
+class Bowman extends Character {
+   constructor(name) {
+     super(name);
+     this.type = "Bowman";
+     this.attack = 25;
+     this.defence = 25;
+   }
+}
+
+class Swordsman extends Character {
+  constructor(name) {
+    super(name);
+    this.type = "Swordsman";
+    this.attack = 40;
+    this.defence = 40;
+  }
+}
+
+class Magician extends Character {
+  constructor(name) {
+    super(name);
+    this.type = "Magician";
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+class Undead extends Character {
+  constructor(name) {
+    super(name);
+    this.type = "Undead";
+    this.attack = 25;
+    this.defence = 25
+  }
+}
+
+class Zombie extends Character {
+  constructor(name) {
+    super(name);
+    this.type = "Zombie";
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+class Daemon extends Character {
+  constructor(name) {
+    super(name);
+    this.type = "Daemon";
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+
--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
-import Character from "../src/add";
+import {Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon} from "../src/add";
 
 test ("test charcter", () => {
-  let petr = new Character("petr", "Bowman");
-  petr.damage(20);
+// тестирование объекта Character
+  let petr = new Character("petr", "Bowman", 25, 25);
+  petr.damage(10);
   const received = petr.health;
-  const expected = 88;
+  const expected = 92.5;
 
   petr.health = -1;
   petr.damage(10);
@@ -13,6 +14,39 @@ test ("test charcter", () => {
 
   expect(received).toEqual(expected);
   expect(received1).toEqual(expected1);
+
+// тестирование дочерних объектов
+  let bow = new Bowman("Bow");
+  const received2 = bow.attack;
+  const expected2 = 25;
+  expect(received2).toEqual(expected2);
+
+  let mad = new Magician("Mow");
+  const received3 = mad.defence;
+  const expected3 = 40;
+  expect(received3).toEqual(expected3);
+
+  let zp = new Zombie("zp");
+  const received4 = zp.type;
+  const expected4 = "Zombie";
+  expect(received4).toEqual(expected4);
+
+  let dae = new Daemon("Dae");
+  const received5 = dae.name;
+  const expected5 = "Dae";
+  expect(received5).toEqual(expected5);
+
+  let sword = new Swordsman("Man");
+  sword.damage(50);
+  const received6 = sword.health;
+  const expected6 = 70;
+  expect(received6).toEqual(expected6);
+
+  let un = new Undead("un");
+  un.damage(50);
+  const received7 = un.health;
+  const expected7 = 62.5;
+  expect(received7).toEqual(expected7);
 })
 
 
